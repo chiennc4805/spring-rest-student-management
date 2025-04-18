9fcf7d0cecd0592c911510b2e443bda9dcc5459d
chiennc4805 <chiennguyen5981@gmail.com>
2025-04-18 17:00:40 +0700

    init project

---
+++ src/main/java/com/myproject/sm/SmApplication.java
@@ -0,0 +1,18 @@
+package com.myproject.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//disable security
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
+
+// @SpringBootApplication
+public class SmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmApplication.class, args);
+	}
+
+}
